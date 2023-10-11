f75f3716318764bf7916b6cbb0fcd11de59f7819
Vimal Kumar <vimal.kumar@spirerecoverysolutions.com>
2023-10-11 21:48:30 +0000

    Changes in ACHStatusEventResponse, also removed th Datetimeconvertor class

--- UsaEPay.NET/Models/Classes/ACHStatusEventResponse.cs
+++ UsaEPay.NET/Models/Classes/ACHStatusEventResponse.cs
@@ -1,156 +1,166 @@
 ﻿using Newtonsoft.Json;
-using RestSharp;
+using System;
 using UsaEPay.NET.Converter;
 
 namespace UsaEPay.NET.Models.Classes
 {
-    public class ACHStatusEventResponse : BaseEventResponse
+    public partial class ACHStatusEvenResponse
     {
+        /// <summary>
+        /// Object type. This will always be transaction.
+        /// </summary>
+        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
+        public string Type { get; set; }
 
         /// <summary>
-        /// Merchant information which triggered the event.
+        /// The timestamp indicating when the event was triggered.
         /// </summary>
-        public class Merchant
-        {
-            /// <summary>
-            /// Unique identifier for the merchant which triggered the event.
-            /// </summary>
-            [JsonProperty("merch_key", NullValueHandling = NullValueHandling.Ignore)]
-            public string MerchKey { get; set; }
-        }
+        [JsonProperty("event_triggered", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset EventTriggered { get; set; }
 
         /// <summary>
-        /// Represents check information associated with the ACH event.
+        /// Describes the type of the event, e.g., "ach.voided."
         /// </summary>
-        public class Check
-        {
-            /// <summary>
-            /// Gets or sets the tracking code for the check.
-            /// </summary>
-            [JsonProperty("trackingcode", NullValueHandling = NullValueHandling.Ignore)]
-            public string TrackingCode { get; set; }
-        }
+        [JsonProperty("event_type", NullValueHandling = NullValueHandling.Ignore)]
+        public string EventType { get; set; }
 
         /// <summary>
-        /// Transaction object related to the event.
+        /// The body of the ACH event. Contains detailed information about the event,
+        /// such as merchant details, transaction object, and changes in values.
         /// </summary>
-        public class TransactionObject
-        {
-            /// <summary>
-            /// This object type will always be transaction.
-            /// </summary>
-            [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
-            public string Type { get; set; }
-
-            /// <summary>
-            /// Unique gateway generated key.
-            /// </summary>
-            [JsonProperty("key", NullValueHandling = NullValueHandling.Ignore)]
-            public string Key { get; set; }
-
-            /// <summary>
-            /// Unique transaction reference number.
-            /// </summary>
-            [JsonProperty("refnum", NullValueHandling = NullValueHandling.Ignore)]
-            public string RefNum { get; set; }
-
-            /// <summary>
-            /// Merchant assigned order ID
-            /// </summary>
-            [JsonProperty("orderid", NullValueHandling = NullValueHandling.Ignore)]
-            public string OrderId { get; set; }
-
-            /// <summary>
-            /// Object which holds all check information
-            /// </summary>
-            [JsonProperty("check", NullValueHandling = NullValueHandling.Ignore)]
-            public Check Check { get; set; }
-
-            /// <summary>
-            /// transaction URI.
-            /// </summary>
-            [JsonProperty("uri", NullValueHandling = NullValueHandling.Ignore)]
-            public string Uri { get; set; }
-        }
+        [JsonProperty("event_body", NullValueHandling = NullValueHandling.Ignore)]
+        public EventBody EventBody { get; set; }
 
         /// <summary>
-        ///  Object containing old field values before ACH status was updated
+        /// Unique identifier for the event.
         /// </summary>
-        public class OldValues
-        {
-            /// <summary>
-            /// Gets or sets the status before the update.
-            /// </summary>
-            [JsonProperty("status", NullValueHandling = NullValueHandling.Ignore)]
-            public string Status { get; set; }
-
-            /// <summary>
-            /// Gets or sets the date when the transaction was settled (if applicable).
-            /// </summary>
-            [JsonProperty("settled", NullValueHandling = NullValueHandling.Ignore)]
-            [JsonConverter(typeof(ParseStringToDateTimeConvertor))]
-            public DateTime? Settled { get; set; }
-        }
-
-        /// <summary>
-        ///  Object containing new field values after ACH status updated
-        /// </summary>
-        public class NewValues
-        {
-            /// <summary>
-            /// Gets or sets the updated status.
-            /// </summary>
-            [JsonProperty("status", NullValueHandling = NullValueHandling.Ignore)]
-            public string Status { get; set; }
-
-            /// <summary>
-            /// Gets or sets the date when the transaction was settled (if applicable).
-            /// </summary>
-            [JsonProperty("settled", NullValueHandling = NullValueHandling.Ignore)]
-            [JsonConverter(typeof(ParseStringToDateTimeConvertor))]
-            public DateTime? Settled { get; set; }
-        }
-
-        /// <summary>
-        /// Logs what fields changed during the update and displays the old and new values.
-        /// </summary>
-        public class Changes
-        {
-            /// <summary>
-            /// Gets or sets the old values before the update.
-            /// </summary>
-            [JsonProperty("old", NullValueHandling = NullValueHandling.Ignore)]
-            public OldValues Old { get; set; }
-
-            /// <summary>
-            /// Gets or sets the new values after the update.
-            /// </summary>
-            [JsonProperty("new", NullValueHandling = NullValueHandling.Ignore)]
-            public NewValues New { get; set; }
-        }
+        [JsonProperty("event_id", NullValueHandling = NullValueHandling.Ignore)]
+        public string EventId { get; set; }
     }
 
-     /// <summary>
+    /// <summary>
     /// Represents the body of an ACH event.
     /// </summary>
-    public class EventBody
+    public partial class EventBody
     {
         /// <summary>
         /// Gets or sets the merchant information triggering the event.
         /// </summary>
         [JsonProperty("merchant", NullValueHandling = NullValueHandling.Ignore)]
-        public ACHStatusEventResponse.Merchant Merchant { get; set; }
+        public Merchant Merchant { get; set; }
 
         /// <summary>
         /// Gets or sets the transaction object related to the event.
         /// </summary>
         [JsonProperty("object", NullValueHandling = NullValueHandling.Ignore)]
-        public ACHStatusEventResponse.TransactionObject Object { get; set; }
+        public Object Object { get; set; }
 
         /// <summary>
         /// Gets or sets the changes in values during the update.
         /// </summary>
         [JsonProperty("changes", NullValueHandling = NullValueHandling.Ignore)]
-        public ACHStatusEventResponse.Changes Changes { get; set; }
+        public Changes Changes { get; set; }
+    }
+
+    /// <summary>
+    /// Logs what fields changed during the update and displays the old and new values.
+    /// </summary>
+    public partial class Changes
+    {
+        /// <summary>
+        /// Gets or sets the old values before the update.
+        /// </summary>
+        [JsonProperty("old", NullValueHandling = NullValueHandling.Ignore)]
+        public ChangeDetails Old { get; set; }
+
+        /// <summary>
+        /// Gets or sets the new values after the update.
+        /// </summary>
+        [JsonProperty("new", NullValueHandling = NullValueHandling.Ignore)]
+        public ChangeDetails New { get; set; }
+    }
+    /// <summary>
+    ///  Object containing old/new field values before ACH status was updated
+    /// </summary>
+    public partial class ChangeDetails
+    {
+        /// <summary>
+        /// Gets or sets the status before/after the update.
+        /// </summary>
+        [JsonProperty("status", NullValueHandling = NullValueHandling.Ignore)]
+        public string Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date when the transaction was settled (if applicable).
+        /// </summary>
+        [JsonProperty("processed", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? Processed { get; set; }
+    }
+
+    /// <summary>
+    /// Merchant information which triggered the event.
+    /// </summary>
+    public partial class Merchant
+    {
+        /// <summary>
+        /// Unique identifier for the merchant which triggered the event.
+        /// </summary>
+        [JsonProperty("merch_key", NullValueHandling = NullValueHandling.Ignore)]
+        public string MerchKey { get; set; }
+    }
+
+    /// <summary>
+    /// Transaction object related to the event.
+    /// </summary>
+    public partial class Object
+    {
+        /// <summary>
+        /// This object type will always be transaction.
+        /// </summary>
+        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Unique gateway generated key.
+        /// </summary>
+        [JsonProperty("key", NullValueHandling = NullValueHandling.Ignore)]
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Unique transaction reference number.
+        /// </summary>
+        [JsonProperty("refnum", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonConverter(typeof(ParseStringToLongConverter))]
+        public long Refnum { get; set; }
+
+        /// <summary>
+        /// Merchant assigned order ID
+        /// </summary>
+        [JsonProperty("orderid", NullValueHandling = NullValueHandling.Ignore)]
+        public string Orderid { get; set; }
+
+        /// <summary>
+        /// Object which holds all check information
+        /// </summary>
+        [JsonProperty("check", NullValueHandling = NullValueHandling.Ignore)]
+        public Check Check { get; set; }
+
+        /// <summary>
+        /// transaction URI.
+        /// </summary>
+        [JsonProperty("uri", NullValueHandling = NullValueHandling.Ignore)]
+        public string Uri { get; set; }
+    }
+
+    /// <summary>
+    /// Represents check information associated with the ACH event.
+    /// </summary>
+    public partial class Check
+    {
+        /// <summary>
+        /// Gets or sets the tracking code for the check.
+        /// </summary>
+        [JsonProperty("trackingcode", NullValueHandling = NullValueHandling.Ignore)]
+        public string Trackingcode { get; set; }
     }
 }
