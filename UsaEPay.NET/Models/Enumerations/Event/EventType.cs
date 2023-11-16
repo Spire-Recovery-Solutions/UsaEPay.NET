835c4ed047418ddaf10f5127199dd5947c52ef31
Vimal Kumar <vimal.kumar@spirerecoverysolutions.com>
2023-11-16 18:20:32 +0000

    removed jsonpropertyname

--- UsaEPay.NET/Models/Enumerations/Event/EventType.cs
+++ UsaEPay.NET/Models/Enumerations/Event/EventType.cs
@@ -18,75 +18,63 @@ namespace UsaEPay.NET.Models.Enumerations.Event
         /// <summary>
         /// ACH status updated to submitted.
         /// </summary>
-        [JsonPropertyName("ach.submitted")] 
         AchSubmitted,
 
         /// <summary>
         /// ACH status updated to settled.
         /// </summary>
-        [JsonPropertyName("ach.settled")] 
         AchSettled,
 
         /// <summary>
         /// ACH status updated to returned.
         /// </summary>
-        [JsonPropertyName("ach.returned")] 
         AchReturned,
 
         /// <summary>
         /// ACH status updated to voided.
-        /// </summary>
-        [JsonPropertyName("ach.voided")] 
+        /// </summary> 
         AchVoided,
 
         /// <summary>
         /// ACH status updated to failed.
         /// </summary>
-        [JsonPropertyName("ach.failed")] 
         AchFailed,
 
         /// <summary>
         /// Note added to ACH transaction.
         /// </summary>
-        [JsonPropertyName("cardupdate.note_added")]
         AchNoteAdded,
 
         // Card Update (CAU) Events
 
         /// <summary>
         /// Card has been queued for update.
-        /// </summary>
-        [JsonPropertyName("cardupdate.created")] 
+        /// </summary> 
         CauCreated,
 
         /// <summary>
         /// Card has been submitted to the processor for update.
         /// </summary>
-        [JsonPropertyName("cardupdate.submitted")] 
         CauSubmitted,
 
         /// <summary>
         /// An updated expiration date has been received.
         /// </summary>
-        [JsonPropertyName("cardupdate.updated_expiration")]
         CauUpdatedExpiration,
 
         /// <summary>
         /// An updated card number has been received.
         /// </summary>
-        [JsonPropertyName("cardupdate.updated_card")] 
         CauUpdatedCard,
 
         /// <summary>
         /// Received a message from the issuer to contact the customer for a new card number.
         /// </summary>
-        [JsonPropertyName("cardupdate.contact_customer")]
         CauContactCustomer,
 
         /// <summary>
         /// Received notification of account closure.
         /// </summary>
-        [JsonPropertyName("cardupdate.account_closed")]
         CauAccountClosed,
 
         // Product Inventory Events
@@ -94,13 +82,11 @@ namespace UsaEPay.NET.Models.Enumerations.Event
         /// <summary>
         /// The inventory has been adjusted after a sale.
         /// </summary>
-        [JsonPropertyName("product.inventory.ordered")]
         ProductInventoryOrdered,
 
         /// <summary>
         /// The inventory has been adjusted in the Products Database.
         /// </summary>
-        [JsonPropertyName("product.inventory.adjusted")]
         ProductInventoryAdjusted,
 
         // Transaction Events
@@ -108,55 +94,46 @@ namespace UsaEPay.NET.Models.Enumerations.Event
         /// <summary>
         /// A transaction sale approved.
         /// </summary>
-        [JsonPropertyName("transaction.sale.success")]
         TransactionSaleSuccess,
 
         /// <summary>
         /// A transaction sale failed - includes declines and errors.
         /// </summary>
-        [JsonPropertyName("transaction.sale.failure")]
         TransactionSaleFailure,
 
         /// <summary>
         /// A transaction sale was voided.
         /// </summary>
-        [JsonPropertyName("transaction.sale.voided")]
         TransactionSaleVoided,
 
         /// <summary>
         /// A transaction sale was captured.
         /// </summary>
-        [JsonPropertyName("transaction.sale.captured")]
         TransactionSaleCaptured,
 
         /// <summary>
         /// A transaction sale was adjusted.
         /// </summary>
-        [JsonPropertyName("transaction.sale.adjusted")]
         TransactionSaleAdjusted,
 
         /// <summary>
         /// A transaction sale was queued.
         /// </summary>
-        [JsonPropertyName("transaction.sale.queued")]
         TransactionSaleQueued,
 
         /// <summary>
         /// A voided transaction sale was unvoided.
         /// </summary>
-        [JsonPropertyName("transaction.sale.unvoided")]
         TransactionSaleUnvoided,
 
         /// <summary>
         /// A transaction refund approved.
         /// </summary>
-        [JsonPropertyName("transaction.refund.success")]
         TransactionRefundSuccess,
 
         /// <summary>
         /// A transaction refund was voided.
         /// </summary>
-        [JsonPropertyName("transaction.refund.voided")]
         TransactionRefundVoided,
 
         // Settlement Batch Events
@@ -164,13 +141,11 @@ namespace UsaEPay.NET.Models.Enumerations.Event
         /// <summary>
         /// A batch settles successfully.
         /// </summary>
-        [JsonPropertyName("settlement.batch.success")]
         SettlementBatchSuccess,
 
         /// <summary>
         /// A batch receives an error when attempting to settle.
         /// </summary>
-        [JsonPropertyName("settlement.batch.failure")]
         SettlementBatchFailure,
     }
 }
